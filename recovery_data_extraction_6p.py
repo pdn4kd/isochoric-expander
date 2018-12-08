5457d55d5d82742b2c5a8d4adac6fb7dd22cfdc2
pdn4kd <Patrick002@live.missouristate.edu>
2018-12-07 19:00:48 -0500

    fix recovering last planet

--- recovery_data_extraction_6p.py
+++ recovery_data_extraction_6p.py
@@ -9,7 +9,7 @@ file_postfix = "65p" # note: columns = 2+(planets*parameters). 5p and fp have 4 
 maxplanets = 10
 
 planetfits_results = open("planetfits_results"+file_postfix+".csv", 'w')
-planetfits_results.write("Star_"+file_postfix+",num_"+file_postfix+",PlanetNumber_"+file_postfix+",per_min_"+file_postfix+",per_mid_"+file_postfix+",per_max_"+file_postfix+",per_err_minus_"+file_postfix+",per_err_plus_"+file_postfix+",per_sigma_minus_"+file_postfix+",per_sigma_plus_"+file_postfix+",tc_min_"+file_postfix+",tc_mid_"+file_postfix+",tc_max_"+file_postfix+",tc_err_minus_"+file_postfix+",tc_err_plus_"+file_postfix+",e_min_"+file_postfix+",e_mid_"+file_postfix+",e_max_"+file_postfix+",e_err_minus_"+file_postfix+",e_err_plus_"+file_postfix+",K_min_"+file_postfix+",K_mid_"+file_postfix+",K_max_"+file_postfix+",K_err_minus_"+file_postfix+",K_err_plus_"+file_postfix+",K_sigma_minus_"+file_postfix+",K_sigma_plus_"+file_postfix+"\n")
+planetfits_results.write("Star_"+file_postfix+",num,PlanetNumber_"+file_postfix+",per_min_"+file_postfix+",per_mid_"+file_postfix+",per_max_"+file_postfix+",per_err_minus_"+file_postfix+",per_err_plus_"+file_postfix+",per_sigma_minus_"+file_postfix+",per_sigma_plus_"+file_postfix+",tc_min_"+file_postfix+",tc_mid_"+file_postfix+",tc_max_"+file_postfix+",tc_err_minus_"+file_postfix+",tc_err_plus_"+file_postfix+",e_min_"+file_postfix+",e_mid_"+file_postfix+",e_max_"+file_postfix+",e_err_minus_"+file_postfix+",e_err_plus_"+file_postfix+",K_min_"+file_postfix+",K_mid_"+file_postfix+",K_max_"+file_postfix+",K_err_minus_"+file_postfix+",K_err_plus_"+file_postfix+",K_sigma_minus_"+file_postfix+",K_sigma_plus_"+file_postfix+"\n")
 
 for i in np.arange(1,len(stars)):
 	if (star_name != stars[i]["HIPnumber"]):
@@ -36,4 +36,21 @@ for i in np.arange(1,len(stars)):
 					
 					planetfits_results.write("\n")
 		star_name = stars[i]["HIPnumber"]
+if (os.path.isfile(star_name+file_postfix+"/"+star_name+file_postfix+"_post_summary.csv")):
+	planets_pd = pd.read_csv(star_name+file_postfix+"/"+star_name+file_postfix+"_post_summary.csv")
+	planets_columns = planets_pd.columns
+	#planetfits_results.write(star_name)
+	for x in np.arange(0, (len(planets_columns)-2)/4, 1):
+		planetfits_results.write(star_name+","+str(stars[i]["PlanetNumber"])+","+str(int(x+1)))
+		planetfits_results.write(","+str(planets_pd[planets_columns[int(x*3)+1]][0])+","+str(planets_pd[planets_columns[int(x*3)+1]][1])+","+str(planets_pd[planets_columns[int(x*3)+1]][2])) # per
+		planetfits_results.write(","+str(planets_pd[planets_columns[int(x*3)+1]][1]-planets_pd[planets_columns[int(x*3)+1]][0])+","+str(planets_pd[planets_columns[int(x*3)+1]][2]-planets_pd[planets_columns[int(x*3)+1]][1]))
+		planetfits_results.write(","+str((planets_pd[planets_columns[int(x*3)+1]][1]-planets_pd[planets_columns[int(x*3)+1]][0])/planets_pd[planets_columns[int(x*3)+1]][1])+","+str((planets_pd[planets_columns[int(x*3)+1]][2]-planets_pd[planets_columns[int(x*3)+1]][1])/planets_pd[planets_columns[int(x*3)+1]][1]))
+		planetfits_results.write(","+str(planets_pd[planets_columns[int(x-1-(len(planets_columns)-2)/4)]][0])+","+str(planets_pd[planets_columns[int(x-1-(len(planets_columns)-2)/4)]][1])+","+str(planets_pd[planets_columns[int(x-1-(len(planets_columns)-2)/4)]][2])) # tc
+		planetfits_results.write(","+str(planets_pd[planets_columns[int(x-1-(len(planets_columns)-2)/4)]][1]-planets_pd[planets_columns[int(x-1-(len(planets_columns)-2)/4)]][0])+","+str(planets_pd[planets_columns[int(x-1-(len(planets_columns)-2)/4)]][2]-planets_pd[planets_columns[int(x-1-(len(planets_columns)-2)/4)]][1]))
+		planetfits_results.write(","+str(planets_pd[planets_columns[int(x*3)+2]][0])+","+str(planets_pd[planets_columns[int(x*3)+2]][1])+","+str(planets_pd[planets_columns[int(x*3)+2]][2])) # e
+		planetfits_results.write(","+str(planets_pd[planets_columns[int(x*3)+2]][1]-planets_pd[planets_columns[int(x*3)+2]][0])+","+str(planets_pd[planets_columns[int(x*3)+2]][2]-planets_pd[planets_columns[int(x*3)+2]][1]))
+		planetfits_results.write(","+str(planets_pd[planets_columns[int(x*3)+3]][0])+","+str(planets_pd[planets_columns[int(x*3)+3]][1])+","+str(planets_pd[planets_columns[int(x*3)+3]][2])) # k
+		planetfits_results.write(","+str(planets_pd[planets_columns[int(x*3)+3]][1]-planets_pd[planets_columns[int(x*3)+3]][0])+","+str(planets_pd[planets_columns[int(x*3)+3]][2]-planets_pd[planets_columns[int(x*3)+3]][1]))
+		planetfits_results.write(","+str((planets_pd[planets_columns[int(x*3)+3]][1]-planets_pd[planets_columns[int(x*3)+3]][0])/planets_pd[planets_columns[int(x*3)+3]][1])+","+str((planets_pd[planets_columns[int(x*3)+3]][2]-planets_pd[planets_columns[int(x*3)+3]][1])/planets_pd[planets_columns[int(x*3)+3]][1]))
+		planetfits_results.write("\n")
 planetfits_results.close()
